f52f53e15c1be99bd0093107454627dea28ec349
Tymek <tymz@concloud.net>
2014-07-08 12:02:44 +0200

    Added Spinner Country List

--- app/src/main/java/snowdog/pl/countrylist/CountrListActivity.java
+++ app/src/main/java/snowdog/pl/countrylist/CountrListActivity.java
@@ -1,7 +1,6 @@
 package snowdog.pl.countrylist;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,15 +8,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+import snowdog.pl.countrylist.model.Country;
 
 
-
-public class CountrListActivity extends Activity {
-
+public class CountrListActivity extends Activity implements AdapterView.OnItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //TODO testowe todo
         setContentView(R.layout.activity_countr_list);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
@@ -46,18 +50,46 @@ public class CountrListActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+
+        private Spinner spCountryList;
+        private ArrayList<Country> lsCountries;
 
         public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_countr_list, container, false);
+            spCountryList = (Spinner) rootView.findViewById(R.id.spinner_countrylist);
+
+            lsCountries = new ArrayList<Country>();
+
+            String[] aryCountryNames;
+            String[] aryCountryCodes;
+            aryCountryNames = getActivity().getResources().getStringArray(R.array.countries);
+            aryCountryCodes = getActivity().getResources().getStringArray(R.array.countries_codes);
+
+            for (int i = 0; aryCountryCodes.length > i; i++) {
+                lsCountries.add(new Country(aryCountryCodes[i], aryCountryNames[i]));
+            }
+
+            spCountryList.setAdapter(new ArrayAdapter<Country>(getActivity(), android.R.layout.simple_spinner_item, lsCountries));
+
             return rootView;
         }
     }
