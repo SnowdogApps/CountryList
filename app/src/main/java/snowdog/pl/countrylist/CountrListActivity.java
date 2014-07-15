693ef18c2f0f05797c060fb83b10707083368cce
chomi3 <omx.jonson@gmail.com>
2014-07-15 11:52:16 +0200

    Reverse Geocoding added

--- app/src/main/java/snowdog/pl/countrylist/CountrListActivity.java
+++ app/src/main/java/snowdog/pl/countrylist/CountrListActivity.java
@@ -2,6 +2,10 @@ package snowdog.pl.countrylist;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.location.Address;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -75,6 +79,50 @@ public class CountrListActivity extends Activity implements AdapterView.OnItemSe
         }
 
         @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            ReverseGeocodingTask reverseGeocodingTask = new ReverseGeocodingTask(getActivity(), new ReverseGeocodingTask.ReverseGeocodingListener() {
+                @Override
+                public void onAddressFound(Address address) {
+                    int positionCounter = 0;
+                    for (Country country : lsCountries) {
+                        if (country.getCode().equals(address.getCountryCode())) {
+                            break;
+                        }
+                        positionCounter++;
+                    }
+                    spCountryList.setSelection(positionCounter);
+                    Log.d(TAG, "onAddressFound address: "+address.toString());
+                }
+
+                @Override
+                public void onAddressNotFound() {
+                    Log.d(TAG, "onAddressNotFound");
+                }
+            });
+            LocationManager lm = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+            Location location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            double longitude = 0;
+            double latitude = 0;
+            if(location != null) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+                Log.d(TAG, "location found using NETWORK_PROVIDER");
+            } else {
+                //Try passive provider
+                location = lm.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+
+                Log.d(TAG, "location found using PASSIVE_PROVIDER");
+            }
+            if(latitude != 0 && longitude != 0) {
+                reverseGeocodingTask.execute(latitude, longitude);
+            }
+        }
+
+
+        @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_countr_list, container, false);
@@ -95,30 +143,30 @@ public class CountrListActivity extends Activity implements AdapterView.OnItemSe
                 if(aryCountryCodes[i].equalsIgnoreCase(locale)) countryId = i;
             }
 
-            spCountryList.setAdapter(new ArrayAdapter<Country>(getActivity(), android.R.layout.simple_spinner_item, lsCountries));
-            spCountryList.setSelection(countryId);
+            ArrayAdapter<Country> adapter = new ArrayAdapter<Country>(getActivity(), android.R.layout.simple_spinner_item, lsCountries);
+            spCountryList.setAdapter(adapter);
+          //  spCountryList.setSelection(countryId);
 
-
-            ArrayAdapter<String> autoCompleteAdapter =
-                    new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, aryCountryNames);
+            ArrayAdapter<Country> autoCompleteAdapter =
+                    new ArrayAdapter<Country>(getActivity(), android.R.layout.simple_list_item_1, lsCountries);
             mAutoCompleteCountries.setAdapter(autoCompleteAdapter);
 
             mAutoCompleteCountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    String selectedCountry = (String) parent.getItemAtPosition(position);
+                    Country selectedCountry = (Country) parent.getItemAtPosition(position);
                     int positionCounter = 0;
                     for (Country country : lsCountries) {
-                        if (country.getName().equals(selectedCountry)) {
+                        if (country.equals(selectedCountry)) {
                             break;
                         }
                         positionCounter++;
                     }
                     spCountryList.setSelection(positionCounter);
-                    Log.d(TAG, "onItemClick: " + positionCounter + " country name: " + selectedCountry);
+                    Log.d(TAG, "onItemClick: " + positionCounter + " country name: " + selectedCountry.toString());
                 }
             });
-            Log.d(TAG, "onCreateView");
+
             return rootView;
         }
     }
